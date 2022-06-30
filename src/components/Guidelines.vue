b57115657977e3b68facb8d8f2fea8a7fad640ac
Raimund Canzler <git@rai-canzler.de>
2022-06-30 15:25:31 +0200

    Fix jumping width in guidelines

--- src/components/Guidelines.vue
+++ src/components/Guidelines.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id="guidelines-section" class="max-w-[90ch]">
+  <section id="guidelines-section" class="w-[90ch]">
     <h2 id="guidelines">Guidelines</h2>
     <p class="mb-3 guidelines">
       Diese Regeln berücksichtigen die Probleme von Menschen mit Legasthenie.
